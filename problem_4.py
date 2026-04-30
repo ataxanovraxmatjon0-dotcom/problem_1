6125d2b8a0636a48e04d377600e5b8d3277ccdd7
Raxmatjon <ataxanovraxmatjon0@gmail.com>
2026-05-01 02:43:13 +0500

    the end problem_4

--- problem_4.py
+++ problem_4.py
@@ -1,1 +1,24 @@
 import json
+
+def best(students):
+    return max(students, key=lambda x: x["grade"])
+
+def worst(students):
+    return min(students, key=lambda x: x["grade"])
+
+def average(students):
+    return sum(x["grade"] for x in students) / len(students)
+
+
+with open("students.json", encoding="utf-8") as f:
+    students = json.load(f)
+
+best_student = best(students)
+worst_student = worst(students)
+avg = average(students)
+
+print(students)
+
+print(f"Eng yaxshi talaba: {best_student['name']} — {best_student['grade']}")
+print(f"Eng past baho: {worst_student['name']} — {worst_student['grade']}")
+print(f"O'rtacha baho: {avg}")
